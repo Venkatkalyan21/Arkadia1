af9e4a59cfa53953159664ba7c662d7b55d4b75e
theogaditya <theogadityahota@gmail.com>
2025-08-22 19:26:17 +0530

    added dockerfiles for backend, bot and frontend ; added docker compose

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SocketProvider } from './context/socketContext';
 import { Layout } from './components/layout/Layout';
